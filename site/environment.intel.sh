7524f884b7b0ee2e23c4770bf542c8b52d145efa
Spencer Clark <Spencer.Clark@noaa.gov>
2025-07-17 10:16:45 -0400

    Revert changes needed for debug-mode issue

--- site/environment.intel.sh
+++ site/environment.intel.sh
@@ -75,8 +75,8 @@ case $hostname in
       module rm gcc
       module load intel-classic/2023.2.0
       module unload cray-libsci
-      module load cray-hdf5/1.12.2.11
-      module load cray-netcdf/4.9.0.9
+      module load cray-hdf5/1.14.3.5
+      module load cray-netcdf/4.9.0.17
       module load craype-hugepages4M
       module load cmake/3.27.9
       module load libyaml/0.2.5
@@ -94,11 +94,8 @@ case $hostname in
       export TEMPLATE=site/intel.mk
       export LAUNCHER=srun
 
-      #need to add this for dynamically linking on GAEA
-      export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
-
       # highest level of AVX support
-      export AVX_LEVEL=-march=core-avx-i
+      export AVX_LEVEL=-march=core-avx2
       echo -e ' '
       module list
       ;;
